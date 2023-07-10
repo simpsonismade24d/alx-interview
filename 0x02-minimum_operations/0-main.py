3e5e1f2b49a4125bea96c815a59a63cd5345269b
simpsonismade24d <simpsonismade@gmail.com>
2023-07-10 13:02:44 -0700

    all files

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 7
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
